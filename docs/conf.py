1b4caa4595ecf03e0f2978edcb11ea1c47382882
philiosi <philiosnaz@gmail.com>
2024-08-30 13:33:01 +0900

    .

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme',
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_copybutton',
 ]
 
 #source_suffix = {
